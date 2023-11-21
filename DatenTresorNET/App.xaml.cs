871b0fc7042f570dfd0853689eabc19e33f01b4d
Gerhard Ahrens <gerhard.ahrens@pta.de>
2023-11-21 19:18:15 +0100

    *

--- DatenTresorNET/App.xaml.cs
+++ DatenTresorNET/App.xaml.cs
@@ -3,12 +3,20 @@
     using System.Configuration;
     using System.Data;
     using System.Windows;
+    using System.Windows.Threading;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-    }
+        public App()
+        {
+            App.Current.DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
 
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+        }
+    }
 }
