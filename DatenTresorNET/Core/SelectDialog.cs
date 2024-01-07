217b2477c1e6960c646eaaeb5c573f3bc2555b48
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-01-07 15:26:10 +0100

    *

---
+++ DatenTresorNET/Core/SelectDialog.cs
@@ -0,0 +1,38 @@
+/*
+ * <copyright file="SelectDialog.cs" company="Lifeprojects.de">
+ *     Class: SelectDialog
+ *     Copyright © Lifeprojects.de 2024
+ * </copyright>
+ *
+ * <author>Gerhard Ahrens - Lifeprojects.de</author>
+ * <email>gerhard.ahrens@lifeprojects.de</email>
+ * <date>07.01.2024 15:19:12</date>
+ * <Project>EasyPrototypingNET</Project>
+ *
+ * <summary>
+ * Beschreibung zur Klasse
+ * </summary>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace DatenTresorNET.Core
+{
+    using System;
+
+    public enum SelectDialog : int
+    {
+        None = 0,
+        AddNewDatabase = 1,
+        SearchDatabase = 2,
+        SelectDatabase = 3,
+        DeleteDatabase = 4,
+        QuestionDlg = 10,
+    }
+}
