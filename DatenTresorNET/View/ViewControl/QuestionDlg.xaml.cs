f7dc2982abb86eb8ae1242ab4672a7232d436b49
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-01-09 11:48:12 +0100

    *

---
+++ DatenTresorNET/View/ViewControl/QuestionDlg.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace DatenTresorNET.View
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaktionslogik für QuestionDlg.xaml
+    /// </summary>
+    public partial class QuestionDlg : UserControl
+    {
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(QuestionDlg), new PropertyMetadata(string.Empty, OnDescriptionChanged));
+
+        public QuestionDlg()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        private static void OnDescriptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is QuestionDlg control)
+            {
+                try
+                {
+                    if (e.NewValue != null)
+                    {
+
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
