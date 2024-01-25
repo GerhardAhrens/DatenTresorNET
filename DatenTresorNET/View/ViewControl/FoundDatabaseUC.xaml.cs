ed808e07113e793fc85f830a917ba3fc2aced305
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-01-25 12:59:22 +0100

    *

---
+++ DatenTresorNET/View/ViewControl/FoundDatabaseUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DatenTresorNET.View.ViewControl
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaktionslogik für FoundDatabaseUC.xaml
+    /// </summary>
+    public partial class FoundDatabaseUC : UserControl
+    {
+        public FoundDatabaseUC()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
